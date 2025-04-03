5e76981dc1e4d13653c0c57b420cbaca17a6fb1b
boonaki <joshdurr17@gmail.com>
2025-04-03 13:17:11 -0400

    adding cloudflare and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,66 +2,68 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight(
+	site: 'https://help.engramapp.com',
+	adapter: cloudflare(),
+	integrations: [starlight({
+		title: '',
+		logo: {
+			src: './src/assets/helplogo.png'
+		},
+		customCss: [
+			'./src/styles/docs.css',
+		],
+		sidebar: [
+			{ label: 'Home', link: '/' },
 			{
-				title: '',
-				logo: {
-					src: './src/assets/helplogo.png'
-				},
-				customCss: [
-					'./src/styles/docs.css',
-				],
-				sidebar: [
-					{ label: 'Home', link: '/' },
-					{
-						label: 'Getting Started',
-						autogenerate: { directory: 'getting-started' },
-					},
-					{
-						label: 'Cards',
-						autogenerate: { directory: 'cards' },
-					},
-					{
-						label: 'Deck Configurations',
-						autogenerate: { directory: 'deck-configs' },
-					},
-					{
-						label: 'User Interface',
-						autogenerate: { directory: 'user-interface' },
-					},
-					{
-						label: 'Import',
-						autogenerate: { directory: 'import' },
-					},
-					// {
-					//   label: 'Linking Notes and Files',
-					//   autogenerate: { directory: 'help/linking-notes' },
-					// },
-					{
-						label: 'LLM Usage',
-						autogenerate: { directory: 'llm-usage' },
-					},
-					{
-						label: 'Engram Sync',
-						autogenerate: { directory: 'sync' },
-					},
-					{
-						label: 'Extending Engram',
-						autogenerate: { directory: 'extending-engram' },
-					},
-					{
-						label: 'Engram',
-						autogenerate: { directory: 'engram' },
-					},
-					{
-						label: 'Licenses and Payment',
-						autogenerate: { directory: 'licenses-and-payment' },
-					},
-				],
-			}
-		),
-	],
+				label: 'Getting Started',
+				autogenerate: { directory: 'getting-started' },
+			},
+			{
+				label: 'Cards',
+				autogenerate: { directory: 'cards' },
+			},
+			{
+				label: 'Deck Configurations',
+				autogenerate: { directory: 'deck-configs' },
+			},
+			{
+				label: 'User Interface',
+				autogenerate: { directory: 'user-interface' },
+			},
+			{
+				label: 'Import',
+				autogenerate: { directory: 'import' },
+			},
+			// {
+			//   label: 'Linking Notes and Files',
+			//   autogenerate: { directory: 'help/linking-notes' },
+			// },
+			{
+				label: 'LLM Usage',
+				autogenerate: { directory: 'llm-usage' },
+			},
+			{
+				label: 'Engram Sync',
+				autogenerate: { directory: 'sync' },
+			},
+			{
+				label: 'Extending Engram',
+				autogenerate: { directory: 'extending-engram' },
+			},
+			{
+				label: 'Engram',
+				autogenerate: { directory: 'engram' },
+			},
+			{
+				label: 'Licenses and Payment',
+				autogenerate: { directory: 'licenses-and-payment' },
+			},
+		],
+	}), sitemap()],
 });
